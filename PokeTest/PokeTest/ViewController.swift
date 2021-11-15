0fb6680ae50b75a6433848241e8a7702f3a6422a
Alfonso Palacios <alfonso.palacios@globant.com>
2021-11-14 19:04:31 -0600

    Initial commit

---
+++ PokeTest/PokeTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PokeTest
+//
+//  Created by Alfonso Palacios Castillo on 14/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
